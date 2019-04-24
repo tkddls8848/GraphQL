f178d9c8bb5dc755684729a903f376d3c5df36b0
tkddls8848 <tkddls8848@gmail.com>
2019-04-24 13:53:00 +0900

    Wrapping a REST API

--- graphql/DB.js
+++ graphql/DB.js
@@ -1,42 +1,9 @@
-export let Movies = [
-    {
-        id:0,
-        name:"Movie1",
-        score:10
-    },
-    {
-        id:1,
-        name:"Movie2",
-        score:10
-    },
-    {
-        id:2,
-        name:"Movie3",
-        score:10
-    }
-];
+const fetch = require('node-fetch');
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const getById = (id) => {
-    const filteredMovie = Movies.filter((Movie) => {return Movie.id === id});
-    return filteredMovie[0]
-}
-
-export const deleteMovie = (id) => {
-    const filteredMovie = Movies.filter((Movie) => Movie.id !== id);
-    if(Movies.length > filteredMovie.length){
-        Movies = filteredMovie;
-        return true;
-    } else {
-        return false;
-    }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${Movies.length + 1}`,
-        name: name,
-        score: score
-    }
-    Movies.push(newMovie);
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
+    
 }
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,13 +1,9 @@
-import {Movies, getById, addMovie, deleteMovie} from "./DB";
+import {Movies, getMovies} from "./DB";
 
 const resolvers = {
     Query : {
         Movies: () => Movies,
-        Movie: (_,{id}) => getById(id)
-    },
-    Mutation :{
-        addMovie:(_,{name,score}) => addMovie(name, score),
-        deleteMovie:(_,{id}) => deleteMovie(id)
+        Movie: (_,{limit, rating}) => getMovies(limit, rating)
     }
 };
 
